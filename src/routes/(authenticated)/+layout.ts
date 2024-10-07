d12f238b63686eaf5e78460dfaf71b3f0a7a4cc1
Weave Dev <weave.dev1@gmail.com>
2024-10-07 17:01:43 +0800

    chore(frontend-scaffolding): configured route groupings

---
+++ src/routes/(authenticated)/+layout.ts
@@ -0,0 +1,7 @@
+// import { redirectIfNot } from '$lib/composables/usePermission'
+
+export const load = () => {
+	// @TODO: intercept route request
+	// redirect to login page if not authenticated
+	// redirectIfNot(false)
+}
