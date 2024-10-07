109354f60418fba64b77aa92f9b4bed20edc3ba5
Weave Dev <weave.dev1@gmail.com>
2024-10-07 21:55:42 +0800

    chore(frontend-scaffolding): minor frontend component refactors

--- src/lib/components/Base/Text/types.ts
+++ src/lib/components/Base/Text/types.ts
@@ -1,44 +1,45 @@
-export enum TextColor {
-	ACCENT = 'text-accent',
-	BASE_100 = 'text-base-100',
-	BASE_200 = 'text-base-200',
-	BASE_300 = 'text-base-300',
-	ERROR = 'text-error',
-	GRAY_800 = 'text-gray-800',
-	INFO = 'text-info',
-	NEUTRAL = 'text-neutral',
-	NEUTRAL_CONTENT = 'text-neutral-content',
-	PRIMARY = 'text-primary',
-	PRIMARY_CONTENT = 'text-primary-content',
-	SECONDARY = 'text-secondary',
-	SUCCESS = 'text-success',
-	WARNING = 'text-warning',
-	WARNING_CONTENT = 'text-warning-content',
-}
+export const TextColor = {
+	ACCENT: 'text-accent',
+	BASE_100: 'text-base-100',
+	BASE_200: 'text-base-200',
+	BASE_300: 'text-base-300',
+	ERROR: 'text-error',
+	GRAY_800: 'text-gray-800',
+	INFO: 'text-info',
+	NEUTRAL: 'text-neutral',
+	NEUTRAL_CONTENT: 'text-neutral-content',
+	PRIMARY: 'text-primary',
+	PRIMARY_CONTENT: 'text-primary-content',
+	SECONDARY: 'text-secondary',
+	SUCCESS: 'text-success',
+	WARNING: 'text-warning',
+	WARNING_CONTENT: 'text-warning-content',
+} as const
 
-export enum TextTag {
-	H1 = 'h1',
-	H2 = 'h2',
-	H3 = 'h3',
-	H4 = 'h4',
-	PARA = 'p',
-	SPAN = 'span',
-}
+export const TextTag = {
+	H1: 'h1',
+	H2: 'h2',
+	H3: 'h3',
+	H4: 'h4',
+	PARA: 'p',
+	SPAN: 'span',
+} as const
 
-export enum TextSize {
-	BASE = 'text-base',
-	LG = 'text-lg',
-	SM = 'text-sm',
-	XL = 'text-xl',
-	XL_2 = 'text-2xl',
-	XL_3 = 'text-3xl',
-	XL_4 = 'text-4xl',
-	XS = 'text-xs',
-}
+export const TextSize = {
+	BASE: 'text-base',
+	LG: 'text-lg',
+	SM: 'text-sm',
+	XL: 'text-xl',
+	XL_2: 'text-2xl',
+	XL_3: 'text-3xl',
+	XL_4: 'text-4xl',
+	XL_6: 'text-6xl',
+	XS: 'text-xs',
+} as const
 
-export enum FontWeight {
-	BOLD = 'font-bold',
-	LIGHT = 'font-light',
-	NORMAL = 'font-normal',
-	SEMIBOLD = 'font-semibold',
-}
+export const FontWeight = {
+	BOLD: 'font-bold',
+	LIGHT: 'font-light',
+	NORMAL: 'font-normal',
+	SEMIBOLD: 'font-semibold',
+} as const
