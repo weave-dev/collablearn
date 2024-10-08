5fb38745796e1bafe524171738a9d5297628bff0
Weave Dev <weave.dev1@gmail.com>
2024-10-08 23:50:17 +0800

    chore(user-permission): refactored checking of user role in userIs function

--- src/routes/(authenticated)/app/+page.svelte
+++ src/routes/(authenticated)/app/+page.svelte
@@ -1,7 +1,26 @@
 <script lang="ts">
+	import { ToastVariant } from '$lib/components/Advanced/Toast/types'
+	import { App } from '$lib/modules/app/stores.js'
 	import { authService } from '$lib/modules/authentication'
+	import { userIs } from '$lib/modules/user-roles/composables/index.js'
+	import { Roles } from '$lib/modules/user-roles/types.js'
 	import { translate } from '$lib/translations'
+	import { onMount } from 'svelte'
 	const auth = authService()
+
+	export let data
+	const { toast } = App
+
+	onMount(() => {
+		if (data.getUserRolesErr) {
+			$toast.fire({
+				message: $translate('user-roles.error.failedToFetch'),
+				variant: ToastVariant.ERROR,
+			})
+		}
+
+		userIs(Roles.ADMIN)
+	})
 </script>
 
 <h1>Welcome</h1>
