0c3cda4920c4c66e00798cd97595f8b0256d4d42
Weave Dev <weave.dev1@gmail.com>
2024-10-08 22:28:21 +0800

    chore(authentication-ui): added separate schemas for admin, and user login, and registration of new user; refactored index login page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,8 @@
 <script lang="ts">
-	import JoyToast from '$lib/components/Advanced/Toast/JoyToast.svelte'
 	import JoyAnchor from '$lib/components/Base/Anchor/JoyAnchor.svelte'
-	import { ButtonSize, ButtonVariant } from '$lib/components/Base/Button'
-	import JoyButton from '$lib/components/Base/Button/JoyButton.svelte'
+	import { ButtonVariant } from '$lib/components/Base/Button'
 	import JoyColumn from '$lib/components/Base/Column/JoyColumn.svelte'
 	import JoyContainer from '$lib/components/Base/Container/JoyContainer.svelte'
-	import JoyIcon from '$lib/components/Base/Icon/JoyIcon.svelte'
-	import { Size } from '$lib/components/Base/Icon/types'
-	import JoyInput from '$lib/components/Base/Input/JoyInput.svelte'
 	import JoyRow from '$lib/components/Base/Row/JoyRow.svelte'
 	import JoyText from '$lib/components/Base/Text/JoyText.svelte'
 	import {
@@ -16,46 +11,16 @@
 		TextSize,
 		TextTag,
 	} from '$lib/components/Base/Text/types'
+	import { formMode } from '$lib/modules/authentication'
+	import { FormMode } from '$lib/modules/authentication/components/types'
+	import RegisterLrn from '$lib/modules/authentication/components/RegisterLrn.svelte'
+	import UserLogin from '$lib/modules/authentication/components/UserLogin.svelte'
 	import { translate } from '$lib/translations'
-	import {
-		BorderRounded,
-		ContainerGap,
-		ContainerPadding,
-		Justify,
-		Shadow,
-	} from '$lib/types'
-	import { superForm } from 'sveltekit-superforms'
+	import { ContainerGap, ContainerPadding, Justify } from '$lib/types'
+	import type { PageData } from './$types'
+	import AdminUserLogin from '$lib/modules/authentication/components/AdminUserLogin.svelte'
 
-	import { userService } from '$lib/modules/authentication'
-	import { ToastVariant } from '$lib/components/Advanced/Toast/types.js'
-	const { registerUser } = userService()
-
-	let isAuthenticating = false,
-		toast: JoyToast
-	export let data
-
-	const { form, enhance, errors, constraints } = superForm(data.validSchema, {
-		dataType: 'json',
-		SPA: true,
-		resetForm: false,
-		validators: data.validator,
-		onUpdate: async ({ form }) => {
-			if (!form.valid) return
-
-			isAuthenticating = true
-			const [err] = await registerUser({
-				...form.data,
-				passwordConfirm: form.data.password_confirmation,
-			})
-			isAuthenticating = false
-
-			if (err) {
-				return toast.fire({ message: err.message, variant: ToastVariant.ERROR })
-			}
-
-			return toast.fire({ message: 'User recorded', variant: ToastVariant.SUCCESS })
-		},
-	})
+	export let data: PageData
 </script>
 
 <div class="bg"></div>
@@ -85,119 +50,20 @@
 			</JoyRow>
 		</JoyColumn>
 
-		<!-- Login form -->
-		<JoyColumn
-			shadow={Shadow.XL}
-			rounded={BorderRounded.LG}
-			padding={ContainerPadding.XL}
-			class="w-[500px] bg-base-100 border"
-		>
-			<JoyText
-				tag={TextTag.H1}
-				size={TextSize.XL_3}
-				weight={FontWeight.BOLD}
-				class="mb-6"
-			>
-				{$translate('common.label.logIn')}
-			</JoyText>
-
-			<form method="post" use:enhance>
-				<JoyContainer col class="bg-transparent relative">
-					<JoyToast bind:this={toast} target="shell" id="route+page" />
-
-					<JoyContainer col gap={ContainerGap.XXS} class="w-full mb-8">
-						<div class="relative w-full">
-							<JoyInput
-								class="w-full group bg-base-200/25 focus-within:bg-transparent"
-								type="text"
-								placeholder="Username"
-								bind:value={$form.lrn}
-								attributes={$constraints.lrn}
-							>
-								<JoyIcon
-									icon="user-circle"
-									slot="labeled-l"
-									size={Size.LG}
-									class="group-focus-within:text-primary transition-colors duration-200"
-								/>
-							</JoyInput>
-							{#if $errors.lrn}
-								<span class="first-letter:capitalize text-error">{$errors.lrn}</span>
-							{/if}
-						</div>
-
-						<div class="relative w-full">
-							<JoyInput
-								class="w-full group bg-base-200/25 focus-within:bg-transparent"
-								type="password"
-								placeholder="Password"
-								bind:value={$form.password}
-								attributes={$constraints.password}
-							>
-								<JoyIcon
-									icon="password-cursor"
-									slot="labeled-l"
-									size={Size.LG}
-									class="group-focus-within:text-primary transition-colors duration-200"
-								/>
-							</JoyInput>
-							{#if $errors.password}
-								<span class="first-letter:capitalize text-error">{$errors.password}</span>
-							{/if}
-						</div>
-
-						<div class="relative w-full">
-							<JoyInput
-								class="w-full group bg-base-200/25 focus-within:bg-transparent"
-								type="password"
-								placeholder="Confirm Password"
-								bind:value={$form.password_confirmation}
-								attributes={$constraints.password_confirmation}
-							>
-								<JoyIcon
-									icon="password-cursor"
-									slot="labeled-l"
-									size={Size.LG}
-									class="group-focus-within:text-primary transition-colors duration-200"
-								/>
-							</JoyInput>
-							{#if $errors.password_confirmation}
-								<span class="first-letter:capitalize text-error"
-									>{$errors.password_confirmation}</span
-								>
-							{/if}
-						</div>
-					</JoyContainer>
-
-					<JoyButton
-						size={ButtonSize.MD}
-						variant={ButtonVariant.PRIMARY}
-						type="submit"
-						disabled={isAuthenticating}
-						class="items-center gap-2 w-full shadow-none focus:shadow-lg focus:shadow-primary/50 z-10"
-					>
-						{#if isAuthenticating}
-							<JoyIcon icon="loading" />
-						{/if}
-
-						{$translate('common.label.signIn')}
-					</JoyButton>
-
-					<div class="border border-neutral border-opacity-25 w-full" />
-
-					<div class="flex justify-center w-full">
-						<span class="-mt-7 bg-base-100 text-center w-1/5 -z-0">or</span>
-					</div>
-					<JoyAnchor
-						plain
-						class="text-center text-sm w-full"
-						label={$translate('common.label.signInAsAdministrator')}
-					/>
-				</JoyContainer>
-			</form>
-
-			<!-- <SuperDebug data={$form} /> -->
-		</JoyColumn>
+		<!-- Register form -->
+		{#if $formMode === FormMode.REGISTER}
+			<RegisterLrn
+				validSchema={data.registerValidSchema}
+				validator={data.registerValidator}
+			/>
+		{:else if $formMode == FormMode.USER_LOGIN}
+			<UserLogin validSchema={data.loginValidSchema} validator={data.loginValidator} />
+		{:else if $formMode == FormMode.ADMIN_LOGIN}
+			<AdminUserLogin
+				validSchema={data.loginAdminValidSchema}
+				validator={data.loginAdminValidator}
+			/>
+		{/if}
 	</JoyContainer>
 </div>
 
