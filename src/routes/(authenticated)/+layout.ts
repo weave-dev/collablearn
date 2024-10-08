aeb05300ec5e1afc9202fe8f4e58e55d31c16af0
Weave Dev <weave.dev1@gmail.com>
2024-10-08 23:51:16 +0800

    chore(user-permission): added loading of user roles in layout load function for authenticated route group

--- src/routes/(authenticated)/+layout.ts
+++ src/routes/(authenticated)/+layout.ts
@@ -1,10 +1,17 @@
 import { redirectIfNot } from '$lib/composables/usePermission'
 
 import { App } from '$lib/modules/app'
+import type { User } from '$lib/modules/authentication'
+import { getUserRoles } from '$lib/modules/user-roles'
+import { tryit } from 'radash'
 
-export const load = () => {
+export const load = async () => {
 	// @TODO: intercept route request
 	// redirect to login page if not authenticated
 	const auth = App.getAuthStore()
 	redirectIfNot(auth.isValid)
+
+	const [getUserRolesErr] = await tryit(getUserRoles)(auth.model as User)
+
+	return { getUserRolesErr }
 }
