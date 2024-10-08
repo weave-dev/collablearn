105de3457dbc197d674944cfb149ba3e8ce881e9
Weave Dev <weave.dev1@gmail.com>
2024-10-08 23:49:35 +0800

    chore(user-permission): configured of setting auth store values after signing in a user

--- src/lib/modules/authentication/services.ts
+++ src/lib/modules/authentication/services.ts
@@ -1,5 +1,5 @@
 import type { RecordAuthResponse } from 'pocketbase'
-import { pb } from './stores'
+import { authStore, pb, user } from './stores'
 import { tick } from 'svelte'
 import type { User, NewUserDTO } from './types'
 import { App } from '$lib/routes/types'
@@ -18,6 +18,9 @@ const signInUser = async (
 
 	try {
 		result = await pb.collection(collection).authWithPassword<User>(username, password)
+		authStore.set(pb.authStore)
+		user.set(pb.authStore.model as User)
+
 		return [undefined, result]
 	} catch (error: unknown) {
 		return [error as Error, undefined]
