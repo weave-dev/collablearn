d114b9f9000abc0e15de125cac363cc4bde6d859
Weave Dev <weave.dev1@gmail.com>
2024-10-08 23:50:50 +0800

    chore(user-permission): refactored collection name and role column relationship in user roles services

--- src/lib/modules/user-roles/services.ts
+++ src/lib/modules/user-roles/services.ts
@@ -1,15 +1,20 @@
 import { pb, type User } from '$lib/modules/authentication'
+import { currentRoles, currentUserRole } from './stores'
 import type { UserRole } from './types'
 
-const collection = 'user_role'
+const collection = 'role_user'
 
 export const getUserRoles = (user: User): Promise<UserRole> => {
 	return new Promise((resolve, reject) => {
 		pb.collection<UserRole>(collection)
 			.getFirstListItem(`user_id = "${user.id}"`, {
-				expand: 'role_id',
+				expand: 'roles',
 			})
-			.then((response) => resolve(response))
+			.then((response) => {
+				currentUserRole.set(response)
+				currentRoles.set(response.expand.roles)
+				resolve(response)
+			})
 			.catch((response) => reject(response))
 	})
 }
