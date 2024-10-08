b5ab86564b6848d6b8b505ad5f402d0c8cc7d87a
Weave Dev <weave.dev1@gmail.com>
2024-10-08 22:26:28 +0800

    chore(authentication-ui): added DTO types for admin, and user login, and registration of new user; added auth localization

--- src/lib/modules/authentication/types.ts
+++ src/lib/modules/authentication/types.ts
@@ -2,13 +2,25 @@ import type { RecordModel } from 'pocketbase'
 
 export interface User extends RecordModel {
 	email: string
+	username: string
 	emailVisibility: boolean
 	lrn: string
 	verified: boolean
 }
 
-export interface UserDTO {
-	lrn: string
+export interface LoginDTO {
+	[key: string]: unknown
 	password: string
+}
+
+export interface AdminUserLoginDTO extends LoginDTO {
+	username: string
+}
+
+export interface UserLoginDTO extends LoginDTO {
+	lrn: string
+}
+
+export interface NewUserDTO extends UserLoginDTO {
 	passwordConfirm: string
 }
