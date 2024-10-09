6d32379c8cff5142e7d5224b299eb0b11dfef0be
Weave Dev <weave.dev1@gmail.com>
2024-10-09 23:06:18 +0800

    chore(sidebars): refactored JoyAvatar to use dicebear avatar

--- src/lib/components/Advanced/Avatar/JoyAvatar.svelte
+++ src/lib/components/Advanced/Avatar/JoyAvatar.svelte
@@ -1,16 +1,19 @@
 <script lang="ts">
 	import JoyRow from '$lib/components/Base/Row/JoyRow.svelte'
-	import JoyText from '$lib/components/Base/Text/JoyText.svelte'
-	import { TextSize, FontWeight } from '$lib/components/Base/Text/types'
-	import { ContainerGap } from '$lib/types/Gap'
-	import { type User } from '$lib/modules/auth/types'
+	import { type User } from '$lib/modules/authentication/types'
+	import { initials } from '@dicebear/collection'
+	import { createAvatar } from '@dicebear/core'
 	let clazz = ''
 	export { clazz as class }
-	export let user: User | undefined = undefined
-	$: avatarClass = `text-ellipsis shrink-0 items-center ${clazz}`
+
+	export let user: User
+	$: avatarClass = `max-w-[2.5rem] max-h-[2.5rem] ${clazz}`
+	$: src = createAvatar(initials, {
+		seed: user.username,
+		size: 200,
+	}).toDataUri()
 </script>
 
-<JoyRow class={avatarClass} gap={ContainerGap.XXS}>
-	<span class="h-8 w-8 rounded-full bg-primary grid place-items-center text-sm">US</span>
-	<JoyText size={TextSize.XS} weight={FontWeight.NORMAL}>{user?.name || 'user12345'}</JoyText>
+<JoyRow class={avatarClass}>
+	<img class="rounded-full w-full h-full" {src} alt="user-avatar" />
 </JoyRow>
