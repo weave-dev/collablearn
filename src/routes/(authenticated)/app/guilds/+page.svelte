a5a20aaf628fe4325eb965c040a290dceade0b7c
Weave Dev <weave.dev1@gmail.com>
2024-10-09 23:05:42 +0800

    chore(sidebars): WIP!: guilds page

---
+++ src/routes/(authenticated)/app/guilds/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { page } from '$app/stores'
+	import JoyContainer from '$lib/components/Base/Container/JoyContainer.svelte'
+</script>
+
+<JoyContainer class="bg-primary/50 w-full">
+	<h1>{$page.url.pathname}</h1>
+</JoyContainer>
