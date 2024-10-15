787a04e2314f635944a69f4fd49c06d44d00d916
Weave Dev <weave.dev1@gmail.com>
2024-10-15 18:58:53 +0800

    chore(accounts-management): added options on base service create method

--- src/lib/modules/base/services.ts
+++ src/lib/modules/base/services.ts
@@ -1,11 +1,12 @@
-import type { RecordListOptions, RecordModel } from 'pocketbase'
+import type { RecordListOptions, RecordModel, RecordOptions } from 'pocketbase'
 import { pb } from '$lib/modules/authentication/'
 
 export const createService = <T = RecordModel,>(collection: string) => {
 	const list = (page: number = 1, limit = 30, options: RecordListOptions = {}) =>
 		pb.collection<T>(collection).getList(page, limit, options)
 
-	const create = (dto: Partial<T>) => pb.collection<T>(collection).create(dto)
+	const create = (dto: Partial<T>, options: RecordOptions = {}) =>
+		pb.collection<T>(collection).create(dto, options)
 	const update = (id: string, dto: Partial<T>, options: RecordListOptions = {}) =>
 		pb.collection<T>(collection).update(id, dto, options)
 
