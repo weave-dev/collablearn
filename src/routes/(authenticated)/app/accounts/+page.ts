725effe128de00c2a6d2c3046ce0cec40055404f
Weave Dev <weave.dev1@gmail.com>
2024-11-07 02:24:22 +0800

    chore(accounts-management): added newAccountSchema for validating new accounts

--- src/routes/(authenticated)/app/accounts/+page.ts
+++ src/routes/(authenticated)/app/accounts/+page.ts
@@ -5,8 +5,7 @@ import type { PageLoad } from './$types'
 import { useAccountFormSchema } from '$lib/modules/accounts/forms/schema'
 
 export const load: PageLoad = async ({ url }) => {
-	const { newAccountSchema } =
-		await useAccountFormSchema('en', url.pathname)
+	const { newAccountSchema } = await useAccountFormSchema('en', url.pathname)
 
 	const newAccountValidator = yup(newAccountSchema)
 
