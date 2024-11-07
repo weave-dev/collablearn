2b36498eae0f9277f1453ddf9720eebc985e0862
Weave Dev <weave.dev1@gmail.com>
2024-11-07 17:01:41 +0800

    chore(accounts-management): minor changes to emailVisibility field in accounts type and added services for fetching and updating accounts

--- src/lib/modules/accounts/services.ts
+++ src/lib/modules/accounts/services.ts
@@ -2,7 +2,7 @@ import { createService } from '$lib/modules/base/services'
 import type { NewUserDTO, User } from '$lib/modules/authentication'
 import { omit, tryit } from 'radash'
 import { accounts } from './stores'
-import type { AccountDTO, AccountProfile, AccountProfileDetails } from './types'
+import type { Account, AccountDTO, AccountProfile, AccountProfileDetails } from './types'
 
 const collection = 'users'
 const service = createService<User>(collection)
@@ -40,17 +40,52 @@ const createUserAccount = async (newAccount: AccountDTO) => {
 	return [undefined, profileCreateResult]
 }
 
-const updateUserAccount = (id: string, userAccount: Partial<User>) => {
-	const data: Partial<User> = {
-		...userAccount,
+const getUserAccount = async (id: string): Promise<[Error?, Account?]> => {
+	const [err, result] = await tryit(service.first)(`id = '${id}'`, {
+		expand: 'account_profiles_via_user_id',
+	})
+
+	if (err) {
+		return [err, undefined]
 	}
 
-	return service.update(id, data)
+	return [undefined, result as Account]
+}
+
+const updateUserAccount = async (
+	id: string,
+	profileId: string,
+	accountDTO: Partial<AccountDTO>
+) => {
+	const data: Partial<User> = {
+		...accountDTO,
+	}
+
+	const [err] = await tryit(service.update)(id, data)
+
+	if (err) return [err, undefined]
+
+	const details = omit(data, [
+		'lrn',
+		'email',
+		'emailVisibility',
+		'password',
+		'passwordConfirm',
+	]) as unknown as AccountProfileDetails
+
+	const [profileUpdateErr, profileUpdateResult] = await tryit(
+		accountProfilesService.update
+	)(profileId, { details })
+
+	if (profileUpdateErr) return [profileUpdateErr, undefined]
+
+	return [undefined, profileUpdateResult]
 }
 
 export const accountsService = () => ({
 	...service,
 	createUserAccount,
+	getUserAccount,
 	updateUserAccount,
 	listUserAccounts,
 })
